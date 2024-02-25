fac702fcaa2f18f9ad34737f209e82dffe3cf8d2
mickel_arroz <111148461+mickel-arroz@users.noreply.github.com>
2024-02-25 19:44:34 -0400

    index, header, section all launches, section about, section success launches... And all is responsive

--- src/services/spaceXFetchLaunch.ts
+++ src/services/spaceXFetchLaunch.ts
@@ -2,27 +2,31 @@ import type { Doc, SpaceXLaunches } from "../types/api";
 
 const spaceXAPI = "https://api.spacexdata.com/v5/launches";
 
+
+
 export const getLastestLaunches = async () => {
 
-    const res = await fetch(`${spaceXAPI}/query`, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    query: {},
-    options: {
-      sort: {
-        date_unix: "desc",
-      },
-      limit: 21,
+  const res = await fetch(`${spaceXAPI}/query`, {
+method: "POST",
+headers: {
+  "Content-Type": "application/json",
+},
+body: JSON.stringify({
+  query: {},
+  options: {
+    sort: {
+      date_unix: "desc",
     },
-  }),
+    limit: 50,
+  },
+}),
 });
 
-    const { docs: launches } = (await res.json()) as SpaceXLaunches;
+  const { docs } = (await res.json()) as SpaceXLaunches;
 
-    return launches;
+  const launches = docs.filter((launch) => launch.links.patch.small);
+
+  return launches;
 
 }
 
@@ -34,5 +38,59 @@ export const getLaunchById = async ({id}: {id: string}) => {
     const launch = (await res.json()) as Doc;
 
     return launch;
+
+}
+
+
+export const getLaunchesSuccess = async () => {
+
+  const res = await fetch(`${spaceXAPI}/query`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: {},
+      options: {
+        sort: {
+          date_unix: "asc",
+        },
+        limit: 100,
+      },
+    }),
+    });
+
+  const { docs: launches  } = (await res.json()) as SpaceXLaunches;
+
+  const successLaunches = launches.filter((launch) => launch.success && launch.links.patch.small);
+
+  return successLaunches;
+
+}
+
+
+export const getAllLaunches = async () => {
+
+  const res = await fetch(`${spaceXAPI}/query`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: {},
+      options: {
+        sort: {
+          date_unix: "asc",
+        },
+        limit: 1000,
+      },
+    }),
+    });
+
+  const { docs  } = (await res.json()) as SpaceXLaunches;
+  
+  const launches = docs.filter((launch) => launch.links.patch.small);
+
+  return launches;
 
 }
